48cc9ff1e5c371fccbae4fa0daa623fc045d8474
a01370626 <30885762+a01370626@users.noreply.github.com>
2020-11-07 10:21:42 -0600

    Add files via upload

--- DRUMS.m
+++ DRUMS.m
@@ -1,47 +1,69 @@
 clc
 clear all
+close all
+
+original_image = imread('NewCam.jpeg');                                                                                        
+grayImage = rgb2gray(original_image);
+[pixelCount,grayLevels] = imhist(grayImage);                                                                                   
+max_value = max(max(grayImage));                                                                                                
+min_value = min(min(grayImage));                                                                                                 
+max_pixelcount = max(pixelCount);                                                                                               
+min_pixelcount = min(pixelCount);                                                                                               
+[~, x] = max(pixelCount);                                                                                                       
+[~, y] = min(pixelCount);                                                                                                       
+level_with_max = grayLevels(x);                                                                                                  
+level_with_min = grayLevels(y);
+
 threshold = 90;
-f= imread('Luchiano.jpg');
-grayImage=rgb2gray(f);
 g= imbinarize(grayImage,threshold/255);
 
-[pixelCount,grayLevels] = imhist(grayImage);    %Calculation of the Histogram.
-max_value = max(max(grayImage));                %The absolute maximum value in the image. 
-min_value = min(min(grayImage));                %The absolute minimum value in the image. 
-max_pixelcount = max(pixelCount);               %Find the maximum pixel count. 
-min_pixelcount = min(pixelCount);               %Find the minimum pixel count. 
-[~, x] = max(pixelCount);                       %Find the gray level at which the largest amount of the pixels are. 
-[~, y] = min(pixelCount);                       %Find the gray level at which the least amount of pixels are. 
-level_with_max = grayLevels(x);                 %Store in a variable the gray level at which the largest amount of the pixels are. 
-level_with_min = grayLevels(y);  
 
-cc=bwconncomp(g);                               %Find the components of the binary image
+
+a = grayImage;                                                                                                                 
+optimal_threshold = graythresh(a);                                                                                                                                                                                                                             %normalized form. 
+b = imbinarize(a,optimal_threshold);                                                                                             
+                      
+cc=bwconncomp(not(b));                               
+stats= regionprops(cc, 'Perimeter','Centroid');
+idx =find([stats.Perimeter]< 6000 &[stats.Perimeter]> 5000); 
+binImage2 =ismember(labelmatrix(cc),idx);
+stats3 = regionprops(binImage2, 'MajorAxisLength','MinorAxisLength', 'Perimeter', 'Area', 'EquivDiameter', 'Centroid')
+centroidsB = cat(1,stats3.Centroid)
+centers = stats3.Centroid; diameters = mean([stats3.MajorAxisLength stats3.MinorAxisLength],2); 
+radii = diameters/2
+
 cc2=bwconncomp(not(g)); 
-stats= regionprops(cc, 'Perimeter');
-stats2= regionprops(cc2, 'Perimeter');%Obtain the perimeter of the components found
-idx =find([stats.Perimeter]> 4200);              %Filter the image for just the components Grande
-                                                %with a perimeter bigger than 360
-binImage2 =ismember(labelmatrix(cc),idx);       %Create a new image filtered
+stats2= regionprops(cc2, 'Perimeter');                                                                                          
+idx2 =find([stats2.Perimeter]< 350 & [stats2.Perimeter]> 315);                                                                                                                                                                                                 
+binImage3 =ismember(labelmatrix(cc2),idx2);                                                                                     
+stats4 = regionprops('table',binImage3, 'MajorAxisLength','MinorAxisLength', 'Perimeter', 'Area', 'EquivDiameter', 'Centroid')
+centroidsH = cat(1,stats4.Centroid)
+centers2 = stats4.Centroid; diameters2 = mean([stats4.MajorAxisLength stats4.MinorAxisLength],2); 
+radii2 = diameters2/2
 
-idx2 =find([stats2.Perimeter]< 350 & [stats2.Perimeter]> 300);             %Filter the image for just the components 
-                                                %with a perimeter bigger than 360
-binImage3 =ismember(labelmatrix(cc2),idx2);      %Create a new image filtered Pequeño
+final= bwlabel(b);                                      
+figure 
 
-stats3 = regionprops('table',binImage2, 'MajorAxisLength','MinorAxisLength', 'Perimeter', 'Area', 'EquivDiameter', 'Centroid') %obtain properties for the image
-stats4 = regionprops('table',binImage3, 'MajorAxisLength','MinorAxisLength', 'Perimeter', 'Area', 'EquivDiameter', 'Centroid') %obtain properties for the image
-                                 
-final= bwlabel(g);  
-
-figure
-subplot(2,2,1);
-imshow(f)
+subplot(2, 3, 1); 
+imshow(original_image);                         
 title(['Original image']);
-subplot(2,2,2);
-imshow(g)
+subplot(2, 3, 2);
+imshow(grayImage);                              
 title(['GrayScale image']);
-subplot(2, 2, 3);
-imshow(binImage2);                              %Display the segmented image image with filter to show big coins
-title('Image filter oil drum');
-subplot(2, 2, 4);
-imshow(binImage3);                              %Display the segmented image image with filter to show small coins
-title('Image filter filling hole');
+subplot(2, 3, 3);
+imshow(b);                              
+title('Binarized');
+subplot(2, 3, 5);
+imshow(binImage2);
+hold on
+plot(centroidsB(:,1),centroidsB(:,2),'r*')
+viscircles(centers,radii);
+hold off
+title('Image filter DRUM');
+subplot(2, 3, 6);
+imshow(binImage3);
+hold on
+plot(centroidsH(:,1),centroidsH(:,2),'r*')
+viscircles(centers2,radii2);
+hold off
+title('Image filter HOLE');
